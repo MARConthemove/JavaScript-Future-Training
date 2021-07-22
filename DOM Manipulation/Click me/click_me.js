3ac78bdf8eda4b7deb0fe797c346c9495ae74591
Marc Gaetcke <marc_onthemove@hotmail.com>
2021-07-22 15:34:40 +0200

    click_me.js done

--- DOM Manipulation/Click me/click_me.js
+++ DOM Manipulation/Click me/click_me.js
@@ -1,6 +1,19 @@
 'use strict'
 
 {
+    const init = () =>
+        $('#click_me').addEventListener('click', handleButtonClick)
+
+    const handleButtonClick = event =>
+        changeButtonTextTo(
+            event.target,
+            event.altKey
+                ? 'Cool, you found an easteregg!!'
+                : 'Hey I like it when you click me!'
+        )
+
+    const changeButtonTextTo = (button, text) => (button.innerHTML = text)
+
     const $ = q => document.querySelector(q)
     const $$ = q => Array.from(document.querySelectorAll(q))
     const $on = (el, ev, fn) => {
@@ -9,4 +22,6 @@
             : el.addEventListener(ev, fn)
         return el
     }
+
+    init()
 }
