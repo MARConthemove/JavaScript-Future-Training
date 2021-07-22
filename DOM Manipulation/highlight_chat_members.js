7351368eea618217f1e3fff4cdc32da7698a00a8
Marc Gaetcke <marc_onthemove@hotmail.com>
2021-07-22 12:59:50 +0200

    improved chat member search function - event listener

--- DOM Manipulation/highlight_chat_members.js
+++ DOM Manipulation/highlight_chat_members.js
@@ -1,7 +1,22 @@
 'use strict'
 
 {
+    const init = () =>
+        $('#member_search input').addEventListener('keyup', event => {
+            console.log(event)
+            updateHighlightingOfChatMembers(event.target.value)
+        })
+
+    const updateHighlightingOfChatMembers = partOfMemberName => {
+        removeHighlightsFromAllChatMembers()
+        highlightChatMembersBy(partOfMemberName)
+    }
+
+    const removeHighlightsFromAllChatMembers = () =>
+        chatMembers().forEach(removeHighlight)
+
     const highlightChatMembersBy = partOfMemberName => {
+        if (partOfMemberName === '') return
         chatMembers()
             .filter(member => doesMemberMatch(partOfMemberName, member))
             .forEach(highlight)
@@ -12,8 +27,10 @@
 
     const chatMembers = () => $$('#chat_members li')
     const highlight = el => el.classList.add('highlighted')
+    const removeHighlight = el => el.classList.remove('highlighted')
 
+    const $ = q => document.querySelector(q)
     const $$ = q => Array.from(document.querySelectorAll(q))
 
-    highlightChatMembersBy('ert')
+    init()
 }
