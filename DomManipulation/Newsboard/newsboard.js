665f2a1b4681382b03c923e6aefb5eba7680887d
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-07-30 10:05:55 +0200

    Working on Newsboard

--- DomManipulation/Newsboard/newsboard.js
+++ DomManipulation/Newsboard/newsboard.js
@@ -1,27 +1,20 @@
 'use strict'
 
 {
+    // initializing js start
     const init = () => {
         showFirstMessage()
         $('[title=next]').addEventListener('click', nextMessage)
         $('[title=prev]').addEventListener('click', prevMessage)
+        $('[title=first]').addEventListener('click', firstMessage)
+        $('[title=last]').addEventListener('click', lastMessage)
     }
 
-    const showFirstMessage = () => showMessageByNumber(1)
-
-    const nextMessage = e => {
-        showMessageByNumber((currentMessageNumber += 1))
-        e.preventDefault()
-    }
-    const prevMessage = e => {
-        showMessageByNumber((currentMessageNumber -= 1))
-        e.preventDefault()
+    // functions progressbar
+    const firstMessage = () => {
+        console.log()
     }
 
-    const showMessageByNumber = messageNumber =>
-        ($('.newsboard_content').innerHTML = messages[messageNumber - 1])
-
-    let currentMessageNumber = 1
 
     init()
 }
