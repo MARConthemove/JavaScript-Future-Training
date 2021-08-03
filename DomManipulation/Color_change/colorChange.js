d14ecadedb687f3e33ed09f1e2f659a55bead603
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-08-03 11:28:18 +0200

    Colorslider and ColorButtons are Setup with background changers

--- DomManipulation/Color_change/colorChange.js
+++ DomManipulation/Color_change/colorChange.js
@@ -1,15 +1,22 @@
 {
     const init = () => {
         buttonEvent()
+        sliderEvent()
     }
 
     // selectors:
     const buttons = () => Array.from(document.querySelectorAll('button'))
     const body = () => document.querySelector('body')
+    const slideControllers = () => Array.from(document.querySelectorAll('[type=range]'))
 
     // eventListener
     const buttonEvent = () => buttons().forEach(element => {
         element.addEventListener('click', changeColorBackground)
+    })
+
+    const sliderEvent = () => slideControllers().forEach(element => {
+        console.log(element)
+        element.addEventListener('mousemove', changeColorFromSlider)
     })
 
     // eventFunctions
@@ -17,6 +24,22 @@
         console.log(event)
         // const buttonId = buttons().indexOf(event.target)
         body().style.backgroundColor = event.target.innerHTML
+    }
+
+    const changeColorFromSlider = event => {
+        // event => console.log(event)
+        console.time()
+        // console.log(event.target.value)
+
+        // init colors
+        let colorRed = slideControllers()[0].value
+        let colorGreen = slideControllers()[1].value
+        let colorBlue = slideControllers()[2].value
+
+        const backgroundColor = `rgb(${colorRed}, ${colorGreen}, ${colorBlue})`;
+        console.log(backgroundColor)
+        body().style.backgroundColor = backgroundColor
+        console.timeEnd()
     }
 
     init()
