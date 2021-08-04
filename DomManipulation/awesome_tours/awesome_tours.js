53b7a03694cddce13e98f3cdf9d4b595764b5e47
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-08-04 08:25:58 +0200

    finished building tour tooltips with JS

--- DomManipulation/awesome_tours/awesome_tours.js
+++ DomManipulation/awesome_tours/awesome_tours.js
@@ -1,7 +1,39 @@
 {
     const init = () => {
+        imagesEvent()
+    }
 
+    // Selectors
+    const imagesSelectorArr = Array.from(document.querySelectorAll('img'))
+    const informationField = document.querySelector('section#info p')
+
+    // EventListener
+    const imagesEvent = () => {
+        imagesSelectorArr.forEach(element =>
+            element.addEventListener('mouseenter', imageFunction))
+    }
+
+    // Functions
+    const imageFunction = (event) => {
+        // country flag
+        const flag =
+            `<img src="${event.target.dataset.countryCode}.png" alt="${event.target.dataset.flagName}">`
+
+        // showplace
+        const showplace = `<h3>${flag} ${event.target.alt}</h3>`
+
+        // description
+        const description = `<p>${event.target.dataset.description}</p>`
+
+        informationField.innerHTML = showplace + description
     }
 
     init()
 }
+
+/*
+    data-description="The Eiffel Tower (/ˈaɪfəl ˈtaʊər/ eye-fəl towr; French: Tour Eiffel French pronunciation: ​[tuʁ‿ɛfɛl] About this sound listen) is a wrought iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower..."
+
+    data-country-code="fr"
+    data-flag-name="French Flag"
+*/
