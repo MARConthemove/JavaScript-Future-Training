cbfe5251f73858ac628cebeb3ddef905d485b342
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-08-06 14:39:14 +0200

    javaScript First part solved

--- DomManipulation/calculator/calculator.js
+++ DomManipulation/calculator/calculator.js
@@ -1,41 +1,59 @@
 {
+    // -- initializing app --
     const init = () => {
         numbersListener()
         operatorsListener()
         equalListener()
+        acListener()
     }
 
-    // querySelectors:
-    const numbersSelectorArr = () => Array.from(document.querySelectorAll('.char.number'))
-
-    const operatorsSelectorArr = () =>
-        Array.from(document.querySelectorAll('.char.operator'))
-
-    const equalSelector = () => document.querySelector('.char.equal')
-
-    // eventListener:
+    // -- eventListener --
     const numbersListener = () => numbersSelectorArr().forEach(element => {
         element.addEventListener('click', numbersFunc)
     })
-
     const operatorsListener = () => operatorsSelectorArr().forEach(element => {
         element.addEventListener('click', operatorsFunc)
     })
-
     const equalListener = () => equalSelector().addEventListener('click', equalFunc)
+    const acListener = () => acSelector().addEventListener('click', acFunc)
 
-    // functions:
+
+    // -- querySelectors --
+    const numbersSelectorArr = () => Array.from(document.querySelectorAll('.char.number'))
+    const operatorsSelectorArr = () => Array.from(document.querySelectorAll(".char.operator:not(#AC)"))
+    const acSelector = () => document.querySelector('#AC')
+    const equalSelector = () => document.querySelector('.char.equal')
+    const calculationSelector = () => document.querySelector('.calculation')
+    const outputSelector = () => document.querySelector('.output')
+
+    // -- functions --
     const numbersFunc = (event) => {
-        (event) => console.log(event)
+        outputSelector().firstElementChild.innerHTML += event.target.innerHTML
     }
 
     const operatorsFunc = (event) => {
-        (event) => console.log(event)
-    }
+        outputSelector().firstElementChild.innerHTML += ` ${event.target.innerHTML} `
 
+    }
     const equalFunc = (event) => {
-        console.log(event)
+        let calculationOperationString = outputSelector().firstElementChild.innerHTML
+
+        let calculationOperationStringNew = calculationOperationString
+          .replace(/[x]/g, "*")
+          .replace(/[÷]/g, "/")
+
+        const calculationOperationStringSolution = outputSelector().firstElementChild.innerHTML = eval(calculationOperationStringNew)
+
+        calculationSelector().firstElementChild.innerHTML =
+            `${calculationOperationString} =`
+
+        return calculationOperationString
+    }
+    const acFunc = () => {
+        calculationSelector().firstElementChild.innerHTML = `Ans = ${equalFunc()}`
+        outputSelector().firstElementChild.innerHTML = ''
     }
 
+    // -- starting app --
     init()
 }
