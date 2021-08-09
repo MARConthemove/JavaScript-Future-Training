11c429ba85de0c7ea85b176d1a1ba7592b55e317
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-08-09 11:25:13 +0200

    implemented new functions

--- DomManipulation/calculator/calculator.js
+++ DomManipulation/calculator/calculator.js
@@ -5,6 +5,7 @@
         operatorsListener()
         equalListener()
         acListener()
+        keyListener()
     }
 
     // -- eventListener --
@@ -16,7 +17,7 @@
     })
     const equalListener = () => equalSelector().addEventListener('click', equalFunc)
     const acListener = () => acSelector().addEventListener('click', acFunc)
-
+    const keyListener = () => document.addEventListener('keydown', keyFunc)
 
     // -- querySelectors --
     const numbersSelectorArr = () => Array.from(document.querySelectorAll('.char.number'))
@@ -25,6 +26,7 @@
     const equalSelector = () => document.querySelector('.char.equal')
     const calculationSelector = () => document.querySelector('.calculation')
     const outputSelector = () => document.querySelector('.output')
+
 
     // -- functions --
     const numbersFunc = (event) => {
@@ -44,8 +46,10 @@
 
         const calculationOperationStringSolution = outputSelector().firstElementChild.innerHTML = eval(calculationOperationStringNew)
 
-        calculationSelector().firstElementChild.innerHTML =
-            `${calculationOperationString} =`
+        calculationSelector().firstElementChild.innerHTML = `${calculationOperationString} =`
+
+        // call history function
+        history(calculationOperationString)
 
         return calculationOperationString
     }
@@ -53,6 +57,45 @@
         calculationSelector().firstElementChild.innerHTML = `Ans = ${equalFunc()}`
         outputSelector().firstElementChild.innerHTML = ''
     }
+
+    const keyFunc = (event) => {
+        console.log(event)
+        console.log(event.key)
+        console.log(typeof event.key)
+
+        const calcNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        const operators = ["+", "-", "*", "/"]
+
+        switch (event.key) {
+            case calcNumbers.toString().includes(event.key):
+                // outputSelector().firstElementChild.innerHTML += event.key
+                numbersFunc(event.key)
+                break
+            case operators.includes(event.key):
+                operatorsFunc(event.key)
+                break
+            case event.key === '=':
+                equalFunc(event.key)
+                break
+            case event.key === 'escape':
+                acFunc()
+                break
+            case event.key === 'ArrowUp' || 'ArrowDown':
+                showHistory()
+                break
+            }
+        }
+
+        let historyArr = []
+        const history = (calcString) => {
+            historyArr.push(calcString)
+            console.log(historyArr)
+            return historyArr
+        }
+
+        const showHistory = () => {
+            historyArr.indexOf()
+        }
 
     // -- starting app --
     init()
