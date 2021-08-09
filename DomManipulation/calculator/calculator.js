3e4854a229d8ac90fde0bbfa50120ca98cd204fd
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-08-09 12:59:44 +0200

    Implemented history function

--- DomManipulation/calculator/calculator.js
+++ DomManipulation/calculator/calculator.js
@@ -30,14 +30,24 @@
 
     // -- functions --
     const numbersFunc = (event) => {
-        outputSelector().firstElementChild.innerHTML += event.target.innerHTML
+        if(event.type === 'keydown') {
+            outputSelector().firstElementChild.innerHTML +=
+              event.key
+        } else {
+            outputSelector().firstElementChild.innerHTML +=
+              event.target.innerHTML
+        }
     }
 
     const operatorsFunc = (event) => {
-        outputSelector().firstElementChild.innerHTML += ` ${event.target.innerHTML} `
-
+        if(event.type === 'keydown') {
+            outputSelector().firstElementChild.innerHTML += ` ${event.key} `
+        } else {
+            outputSelector().firstElementChild.innerHTML += ` ${event.target.innerHTML} `
+        }
     }
-    const equalFunc = (event) => {
+
+    const equalFunc = () => {
         let calculationOperationString = outputSelector().firstElementChild.innerHTML
 
         let calculationOperationStringNew = calculationOperationString
@@ -53,49 +63,95 @@
 
         return calculationOperationString
     }
+
     const acFunc = () => {
         calculationSelector().firstElementChild.innerHTML = `Ans = ${equalFunc()}`
         outputSelector().firstElementChild.innerHTML = ''
     }
 
     const keyFunc = (event) => {
-        console.log(event)
-        console.log(event.key)
-        console.log(typeof event.key)
+        // console.log(event)
+        // console.log(event.key)
+
 
         const calcNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         const operators = ["+", "-", "*", "/"]
 
-        switch (event.key) {
+        switch (true) {
             case calcNumbers.toString().includes(event.key):
                 // outputSelector().firstElementChild.innerHTML += event.key
-                numbersFunc(event.key)
+                numbersFunc(event)
                 break
             case operators.includes(event.key):
-                operatorsFunc(event.key)
+                operatorsFunc(event)
                 break
             case event.key === '=':
                 equalFunc(event.key)
                 break
-            case event.key === 'escape':
+            case event.key === 'Escape':
                 acFunc()
                 break
-            case event.key === 'ArrowUp' || 'ArrowDown':
-                showHistory()
+            case event.key === 'Enter':
+                equalFunc()
                 break
+            case event.key === 'ArrowUp' || event.key === 'ArrowDown':
+                showHistory(event)
+                break
+        }
+    }
+
+    let historyArr = []
+    let actualHistoryPos = -1
+
+    const history = (calcString) => {
+        historyArr.push(calcString)
+        // console.log(historyArr)
+        // return historyArr
+    }
+
+    const showHistory = (event) => {
+        if(historyArr.length === 0) {return}
+
+        if(historyArr.length === 1) {
+            outputSelector().firstElementChild.innerHTML = historyArr[0]
+            actualHistoryPos = 0
+            return
+        }
+
+        let lastIndex = historyArr.length - 1
+
+        // First time user activate key event
+        if(actualHistoryPos === -1) {
+            if(event.key === 'ArrowDown') {
+                return
+            } else if(event.key === 'ArrowUp') {
+                outputSelector().firstElementChild.innerHTML = historyArr[lastIndex - 1]
+                actualHistoryPos = lastIndex - 1
+                return
             }
         }
+        console.log(historyArr, actualHistoryPos)
 
-        let historyArr = []
-        const history = (calcString) => {
-            historyArr.push(calcString)
-            console.log(historyArr)
-            return historyArr
+        if (event.key === "ArrowDown") {
+            if(actualHistoryPos === lastIndex) {
+                return
+            }
+            outputSelector().firstElementChild.innerHTML = historyArr[actualHistoryPos + 1]
+            actualHistoryPos += 1
+
+        } else if (event.key === "ArrowUp") {
+            if(actualHistoryPos === 0) {
+                return
+            }
+            outputSelector().firstElementChild.innerHTML = historyArr[actualHistoryPos - 1]
+            actualHistoryPos -= 1
         }
 
-        const showHistory = () => {
-            historyArr.indexOf()
-        }
+
+
+
+
+    }
 
     // -- starting app --
     init()
