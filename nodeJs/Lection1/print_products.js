a4c9ca3eeda2424d96a463ce40dfa03305614585
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-08-10 10:11:40 +0200

    started a little project with node.js

---
+++ nodeJs/Lection1/print_products.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// Import Node.js module - fs (file system)
+const fs = require('fs')
+
+// Function to read the products.csv file + encoding type (utf-8)
+const data = fs.readFileSync('products.csv', 'UTF8')
+
+// split data in rows
+products = data.split('\n')
+products.shift()
+
+console.log(data)
