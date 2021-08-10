e999b09f558528e4f55a1b58a5fd82a8a0fbad41
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-08-10 11:24:07 +0200

    finished read in function

--- nodeJs/Lection1/print_products.js
+++ nodeJs/Lection1/print_products.js
@@ -3,27 +3,30 @@
 // Import Node.js module - fs (file system)
 const fs = require('fs')
 
-// Function to read the products.csv file + encoding type (utf-8)
-const data = fs.readFileSync('products.csv', 'UTF8')
-
-// split data in rows
-const products = data.split('\n')
-
-// removing the headers
-products.shift()
-
 // function to modify the output
 const recordToHTML = record => {
     const fields = record.split(',')
+
     let html = `
         <h1>${fields[0]}</h1>
         <p>${fields[1]}</p>
         <p>Price: EUR ${fields[3]}</p>`
+
+    if (Number(fields[2]) <= 5) {
+        html += `<p>Last items in stock!</p>`
+    }
+
     return html
 }
 
-const pages = products.filter(row => row !== '').map(recordToHTML)
+// Function to read the products.csv file + encoding type (utf-8)
+fs.readFile('products.csv', 'UTF8', (error, data) => {
+    const products = data.split('\n')
+    products.shift() // removing header
 
-pages.forEach(page => console.log(page))
+    const pages = products.filter(row => row !== '').map(recordToHTML)
+
+    pages.forEach(page => console.log(page))
+})
 
 console.log('ready')
