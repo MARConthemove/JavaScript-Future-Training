25c056de6ef064adf161ccfaefaf75a339a6dcab
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-08-10 11:51:19 +0200

    implemented filter functions for csv files

---
+++ nodeJs/Lection1/Ü2.js
@@ -0,0 +1,35 @@
+'use strict'
+
+let fields = []
+
+const mixedCSV =
+    '"very big, soft computer mouse",' + '"the cutest peripheral ever",10,39.90'
+
+let index = 0
+let state = 'outside'
+
+mixedCSV.split('').forEach(char => {
+    if (state === 'quoted') {
+        fields[index] += char
+        if (char === '"') {
+            state = 'outside'
+            index += 1
+        }
+    } else if (state === 'unquoted') {
+        if (char === ',') {
+            state = 'outside'
+            index += 1
+        } else {
+            fields[index] += char
+        }
+    } else if (state === 'outside') {
+        fields[index] = char
+        if (char === '"') {
+            state = 'quoted'
+        } else if (char !== ',') {
+            state = 'unquoted'
+        }
+    }
+})
+
+console.log(fields)
