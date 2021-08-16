645c05d194896871ce2ccc6c9727398a1b66fa60
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-08-16 07:47:34 +0200

    made some changes to the webserver

--- nodeJs/server/server.js
+++ nodeJs/server/server.js
@@ -5,12 +5,18 @@ const http = require("http")
 const ip = "127.0.0.1"
 const port = 8081
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader("Content-Type", "text/plain")
-    res.end("Hello World\n")
+const server = http.createServer((request, response) => {
+    // console.log(response)
+    response.statusCode = 404
+    response.setHeader("Content-Type", "text/plain")
+    response.end("Hello World\n")
+
+    // request "Event" -> see possibilities
+    // console.log(request)
+    console.log(request.headers['user-agent'])
 })
 
 server.listen(port, ip, () => {
     console.log(`Server running at http://${ip}:${port}/`)
 })
+
