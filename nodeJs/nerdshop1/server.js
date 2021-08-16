25a65330be908d32eb97ab213fbb63962083e49c
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-08-16 11:51:40 +0200

    small changes in logic for routing

--- nodeJs/nerdshop1/server.js
+++ nodeJs/nerdshop1/server.js
@@ -7,23 +7,39 @@ const app = express()
 const ip = '127.0.0.1'
 const port = 8081
 
+// search in public folder
 app.use(express.static('public'))
 
 fs.readFile('products.csv', 'UTF8', (error, data) => {
     let products = data.split('\n')
     products.shift()
 
+    // map sends every element as an argument to the recordToHTML function
+    // pages builds array of every row with HTML
     let pages = products.filter(row => row !== '').map(recordToHTML)
 
-    app.get('/product/:ix', (req, res) => {
-        res.send(pages[req.params.ix])
+    // res.send -> response -> output in html page
+    app.get('/product', (req, res) => {
+        res.send(pages[req.query.ix])
     })
 
+    // for routes send information via formular to the server
+    // app.post(....)
+
+    app.get('/book/:title', (req, res) => {
+        res.send(req.params.title)
+        console.log(req.params.title)
+    })
+
+    // server listens to requests
+    // func only to show user the server is running now!
     app.listen(port, ip, () => {
         console.log(`Server running at http://${ip}:${port}/`)
     })
 })
 
+// record -> string (row) from pages array
+// smartphone case with built-in screen,never miss a message,20,39.90 (for ex)
 const recordToHTML = record => {
     const fields = record.split(',')
 
@@ -36,5 +52,6 @@ const recordToHTML = record => {
         html += `<p>Last items in stock!</p>`
     }
 
+    // .map() -> return value
     return html
 }
