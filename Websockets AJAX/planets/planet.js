817ca9b12c304f347bffe8368b183151ea4d01e4
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-08-20 10:39:24 +0200

    finished planet project

--- Websockets AJAX/planets/planet.js
+++ Websockets AJAX/planets/planet.js
@@ -5,7 +5,7 @@ const URL = 'https://solar.api.webmasters-akademie.dev/bodies/'
 
 const PlanetNames = {
     Earth: 'terre',
-    Mars: 'mars',
+    Venus: 'venus',
 }
 
 // selectors:
@@ -23,28 +23,27 @@ const findPlanet = planet => {
     // fetching data from API -> promise:
     fetch(`${URL}${PlanetNames[planet]}`)
         .then(res => res.json()) // transformation of data to json
-        .then(res => showPlanetData(res)) //create planet information
+        .then(res => document.querySelector('#information-field').innerHTML = showPlanetData(res)) //create planet information
         .catch(err => console.log('There was an error: ' + err))
 }
 
 const showPlanetData = res => {
-    console.log(res.moons)
+    console.log(res)
 
-    // tap information from API
-    const name = document.createElement('p')
-    name.innerHTML = res.name
+    const planetString = `
+        <p>${res.name}</p>
+        <p>${showMoons(res.moons)}</p>
+        <p>${res.axialTilt}</p>
+        <p>${res.equaRadius}</p>`
 
-    console.log(name) // testing HTML p-tag
+    return planetString
+}
 
-    const moons = document.createElement('p')
-    moons.innerHTML = res.moons
-
-    const axialTilt = document.createElement('p')
-    axialTilt.innerHTML = res.axialTilt
-
-    const equatorRadius = document.createElement('p')
-    equatorRadius.innerHTML = res.equaRadius
-
+const showMoons = moons => {
+    let moonString = ''
+    if (moons === null) { return ''}
+        moons.forEach(moon => moonString += ' ' + moon.moon)
+        return moonString
 }
 
 console.log(planet_list)
