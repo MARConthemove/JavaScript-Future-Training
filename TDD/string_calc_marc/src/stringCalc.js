ba3dd754f45e80e6770c5104d2867b5beae62ada
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-08-28 22:37:55 +0200

    tdd string calc

--- TDD/string_calc_marc/src/stringCalc.js
+++ TDD/string_calc_marc/src/stringCalc.js
@@ -1,6 +1,12 @@
 'use strict'
 
-const stringCalc = str => sum(str.split(',').map(Number).filter(n => n <= 1000))
+const stringCalc = (str) =>
+    sum(
+        str
+            .split(/,|\n/g)
+            .map(Number)
+            .filter((n) => n <= 1000)
+    )
 const sum = (numbers) => numbers.reduce(add)
 const add = (a, b) => a + b
 
