fd84acb6894099787a814c7a98066dc937251a59
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-08-30 14:26:00 +0200

    greetings challenge finished. two parts are still up for implementation

--- TDD/greetings/src/greet.js
+++ TDD/greetings/src/greet.js
@@ -1,5 +1,38 @@
+const expect = require('must')
+
 const greet = (name) => {
-    return `Hello, ${name}`
+    // empty or not a string
+    if (typeof name !== 'string' || name.length === 0) {
+        return 'Hello, my friend'
+    }
+
+    // only one name
+    if (name.split(',').length === 1) return `Hello, ${name}`
+
+    // two names -> array and controlling via indexes
+    if (name.split(",").length === 2) {
+        return `Hello ${name.split(",")[0].trim()} and ${name.split(",")[1].trim()}`
+    }
+
+    // more than two names, seperated by comma
+    if (name.length >= 2) {
+        let arrayNames = name.replace(/\s/g, '').split(',')
+        let firstName = arrayNames[0]
+        let middleNames = arrayNames.slice(1, arrayNames.length - 1)
+        let lastName = arrayNames[arrayNames.length - 1]
+
+        return `Hello ${firstName}, ${middleNames.join(', ')} and ${lastName}`
+    }
+
+    // capitalized
+    if (isCapitalized(name)) return `HELLO ${name.toUpperCase()}`
+}
+
+const isCapitalized = (name) => {
+    // creating an array from name
+    const nameArray = name.split('')
+    // checking if every letter is upperCase
+    nameArray.every(letter => letter === letter.toUpperCase()) //-> true, false
 }
 
 module.exports = { greet }
--- TDD/greetings/src/greet.spec.js
+++ TDD/greetings/src/greet.spec.js
@@ -5,4 +5,26 @@ describe("greet", () => {
     it('should say "Hello, ..." to the user', () => {
         expect(greet("Heribert")).to.be.equal("Hello, Heribert")
     })
+
+    it('should check if input is not a string and give back "Hello, my friend"', () => {
+        expect(greet()).to.be.equal('Hello, my friend')
+        // expect(greet(1)).to.be.equal('Hello, my friend')
+    })
+
+    it('should return "Hello, my friend", if input was empty', () => {
+        expect(greet()).to.be.equal('Hello, my friend')
+    })
+
+    it('should return with 2 names, both names with hello', () => {
+        expect(greet('Heribert, Goldy')).to.be.equal('Hello Heribert and Goldy')
+    })
+
+    it('should return with more Names everyname', () => {
+        expect(greet('Heribert, Goldy, Ladislaus')).to.be.equal(
+            'Hello Heribert, Goldy and Ladislaus')
+    })
+
+    it('should return capitalized if screaming', () => {
+        expect(greet('HERIBERT')).to.be.equal('HELLO HERIBERT')
+    })
 })
