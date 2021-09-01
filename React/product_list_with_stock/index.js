4e882773293e52ffc01ea4fcc5e4164574ffdc14
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2021-09-01 11:10:44 +0200

    created variable stock colors

--- React/product_list_with_stock/index.js
+++ React/product_list_with_stock/index.js
@@ -1,18 +1,23 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+// work-arround hot-reloading
+if (module.hot) {
+    module.hot.accept()
+}
+
 const App = () => (
     <div>
         <h2>Product List</h2>
         <ul>
             <li>
-                <Product name="Hot Binary Heat Changing Mug" />
+                <Product name="Hot Binary Heat Changing Mug" stock="20"/>
             </li>
             <li>
-                <Product name="3D Printing Pen" />
+                <Product name="3D Printing Pen" stock="2"/>
             </li>
             <li>
-                <Product name="Klingon Letter Opener" />
+                <Product name="Klingon Letter Opener" stock="15"/>
             </li>
         </ul>
     </div>
@@ -20,7 +25,12 @@ const App = () => (
 
 const Product = (props) => (
     <div>
-        <h3>{props.name}</h3>
+        <h3 className={props.stock < 4 && "stock_critical"}>{props.name}</h3>
+        {props.stock < 4 && (
+            <sub className="stock_critical">
+                (only a few copies still available)
+            </sub>
+        )}
         <p>
             Nulla qui voluptate esse id aliquip duis et tempor quis officia
             veniam. Et cupidatat ut sit eiusmod eiusmod duis tempor et
